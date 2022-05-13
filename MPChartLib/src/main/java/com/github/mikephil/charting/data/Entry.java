2f5e2404907e938a15e2a822c144c699da09d8e7
YeRyeonHur <68859362+YeRyeonHur@users.noreply.github.com>
2022-05-13 13:16:39 +0900

    Update Entry.java

--- MPChartLib/src/main/java/com/github/mikephil/charting/data/Entry.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/data/Entry.java
@@ -1,23 +1,23 @@
-
-package com.github.mikephil.charting.data;
+package com.example.logviewer2;
 
 import android.graphics.drawable.Drawable;
 import android.os.Parcel;
 import android.os.ParcelFormatException;
 import android.os.Parcelable;
 
+import com.github.mikephil.charting.data.BaseEntry;
 import com.github.mikephil.charting.utils.Utils;
 
 /**
  * Class representing one entry in the chart. Might contain multiple values.
  * Might only contain a single value depending on the used constructor.
- * 
+ *
  * @author Philipp Jahoda
  */
 public class Entry extends BaseEntry implements Parcelable {
 
     /** the x value */
-    private float x = 0f;
+    private String x = "";
 
     public Entry() {
 
@@ -29,7 +29,7 @@ public class Entry extends BaseEntry implements Parcelable {
      * @param x the x value
      * @param y the y value (the actual value of the entry)
      */
-    public Entry(float x, float y) {
+    public Entry(String x, float y) {
         super(y);
         this.x = x;
     }
@@ -41,7 +41,7 @@ public class Entry extends BaseEntry implements Parcelable {
      * @param y the y value (the actual value of the entry)
      * @param data Spot for additional data this Entry represents.
      */
-    public Entry(float x, float y, Object data) {
+    public Entry(String x, float y, Object data) {
         super(y, data);
         this.x = x;
     }
@@ -53,7 +53,7 @@ public class Entry extends BaseEntry implements Parcelable {
      * @param y the y value (the actual value of the entry)
      * @param icon icon image
      */
-    public Entry(float x, float y, Drawable icon) {
+    public Entry(String x, float y, Drawable icon) {
         super(y, icon);
         this.x = x;
     }
@@ -66,32 +66,32 @@ public class Entry extends BaseEntry implements Parcelable {
      * @param icon icon image
      * @param data Spot for additional data this Entry represents.
      */
-    public Entry(float x, float y, Drawable icon, Object data) {
+    public Entry(String x, float y, Drawable icon, Object data) {
         super(y, icon, data);
         this.x = x;
     }
 
     /**
      * Returns the x-value of this Entry object.
-     * 
+     *
      * @return
      */
-    public float getX() {
+    public String getX() {
         return x;
     }
 
     /**
      * Sets the x-value of this Entry object.
-     * 
+     *
      * @param x
      */
-    public void setX(float x) {
+    public void setX(String x) {
         this.x = x;
     }
 
     /**
      * returns an exact copy of the entry
-     * 
+     *
      * @return
      */
     public Entry copy() {
@@ -103,7 +103,7 @@ public class Entry extends BaseEntry implements Parcelable {
      * Compares value, xIndex and data of the entries. Returns true if entries
      * are equal in those points, false if not. Does not check by hash-code like
      * it's done by the "equals" method.
-     * 
+     *
      * @param e
      * @return
      */
@@ -115,7 +115,7 @@ public class Entry extends BaseEntry implements Parcelable {
         if (e.getData() != this.getData())
             return false;
 
-        if (Math.abs(e.x - this.x) > Utils.FLOAT_EPSILON)
+        if (e.x.compareTo(this.x) > Utils.FLOAT_EPSILON)
             return false;
 
         if (Math.abs(e.getY() - this.getY()) > Utils.FLOAT_EPSILON)
@@ -139,7 +139,7 @@ public class Entry extends BaseEntry implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeFloat(this.x);
+        dest.writeString(this.x);
         dest.writeFloat(this.getY());
         if (getData() != null) {
             if (getData() instanceof Parcelable) {
@@ -154,7 +154,7 @@ public class Entry extends BaseEntry implements Parcelable {
     }
 
     protected Entry(Parcel in) {
-        this.x = in.readFloat();
+        this.x = in.readString();
         this.setY(in.readFloat());
         if (in.readInt() == 1) {
             this.setData(in.readParcelable(Object.class.getClassLoader()));
